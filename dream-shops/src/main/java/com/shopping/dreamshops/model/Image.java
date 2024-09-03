6600db75954e8b1695a68d312b5c04cb3a93d4ae
psierrajs <psierrajs@gmail.com>
2024-09-03 16:45:34 +0200

    Add attributes to Image class including id, fileName, fileType, image, downloadUrl, and product

--- dream-shops/src/main/java/com/shopping/dreamshops/model/Image.java
+++ dream-shops/src/main/java/com/shopping/dreamshops/model/Image.java
@@ -1,5 +1,14 @@
 package com.shopping.dreamshops.model;
 
+import java.sql.Blob;
+
 public class Image {
+	private Long id;
+	private String fileName;
+	private String fileType;
+	private Blob image;
+	private String downloadUrl;
+	
+	private Product product;
 
 }
