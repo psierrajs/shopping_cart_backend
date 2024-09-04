ca2cd6db595c0e7f19e05a52a55bc8fb28e9f52f
psierrajs <psierrajs@gmail.com>
2024-09-04 12:31:04 +0200

    Add Lombok and JPA annotations to the Product class and configure one-to-many relationship with Image

--- dream-shops/src/main/java/com/shopping/dreamshops/model/Product.java
+++ dream-shops/src/main/java/com/shopping/dreamshops/model/Product.java
@@ -3,8 +3,25 @@ package com.shopping.dreamshops.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String brand;
@@ -14,6 +31,7 @@ public class Product {
 	
 	private Category category;
 	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true )
 	private List<Image> images;
 
 }
