e9dc884545147e6297a4823a4cbe5fc9c76ec65f
psierrajs <psierrajs@gmail.com>
2024-09-04 12:38:41 +0200

    Add JPA one-to-many relationship annotation to Category class

--- dream-shops/src/main/java/com/shopping/dreamshops/model/Category.java
+++ dream-shops/src/main/java/com/shopping/dreamshops/model/Category.java
@@ -2,10 +2,13 @@ package com.shopping.dreamshops.model;
 
 import java.util.List;
 
+import jakarta.persistence.OneToMany;
+
 public class Category {
 	private Long id;
 	private String name;
 	
+	@OneToMany(mappedBy = "category")
 	private List<Product> products;
 	
 
