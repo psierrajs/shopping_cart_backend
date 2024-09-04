8c0bb398ca074c5fbff7a90987cc65aa62a5ff96
psierrajs <psierrajs@gmail.com>
2024-09-04 12:48:41 +0200

    Add Lombok annotations to Category class for constructors, getters, and setters

--- dream-shops/src/main/java/com/shopping/dreamshops/model/Category.java
+++ dream-shops/src/main/java/com/shopping/dreamshops/model/Category.java
@@ -3,7 +3,15 @@ package com.shopping.dreamshops.model;
 import java.util.List;
 
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 	private Long id;
 	private String name;
