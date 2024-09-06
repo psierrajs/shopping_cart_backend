a2475a84df1b228214106b06725d1d689ad43bc9
psierrajs <psierra.js@gmail.com>
2024-09-06 10:03:14 +0200

    Add product service methods to IProductService interface
    - Defined methods in IProductService for core product operations:
      - addProduct, getProductById, deleteProductById, updateProduct.
      - Added methods to retrieve products by category, brand, name, and
    combinations of these.
      - Included method to count products by brand and name.
    - These methods set the structure for managing product-related business
    logic.

--- dream-shops/src/main/java/com/shoppping/dreamshops/service/product/IProductService.java
+++ dream-shops/src/main/java/com/shoppping/dreamshops/service/product/IProductService.java
@@ -1,5 +1,20 @@
 package com.shoppping.dreamshops.service.product;
 
+import java.util.List;
+
+import com.shoppping.dreamshops.model.Product;
+
 public interface IProductService {
 
+	Product addProduct(Product product);
+	Product getProductById(Long id);
+	void deleteProductById(Long id);
+	void updateProduct(Product product, Long productId);
+	List<Product> getAllProducts();
+	List<Product> getProductsByCategory(String category);
+	List<Product> getProductByBrand(String brand);
+	List<Product> getProductsByCategoryAndBrand(String category, String brand);
+	List<Product> getProductsByName(String name);
+	List<Product> getProductsByBrandAndName(String brand, String name);
+	Long countProductsByBrandAndName(String brand, String name);
 }
